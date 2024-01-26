d970f224f6ce44bb01024bcec50ae688f9cb3f4e
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-01-26 19:45:46 +0530

    more about objects

--- 02 Basics/02_Objects.js
+++ 02 Basics/02_Objects.js
@@ -50,3 +50,62 @@ JsUser.greetingTwo = function () {
 console.log(JsUser.greeting); // [Function (anonymous)]
 console.log(JsUser.greeting()); // Hello js User
 console.log(JsUser.greetingTwo()); // Hello js User, Mohit
+
+//* Singleton
+
+// const tinderUser = new Object();
+const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "Mohit";
+tinderUser.loggedIn = false;
+
+console.log(tinderUser); // { id: '123abc', name: 'Mohit', loggedIn: false }
+
+const regularUser = {
+   email: "abc@gmail.com",
+   fullName: {
+      userFullName: {
+         firstName: "Mohit",
+         lastName: "Maithani",
+      },
+   },
+};
+
+console.log(regularUser.fullName.userFullName.firstName); // Mohit
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj3 = { 5: "a", 6: "b" };
+
+// const obj3 = {obj1, obj2};
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+const obj4 = { ...obj1, ...obj2, ...obj3 };
+console.log(obj4); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const users = [
+   {
+      id: 1,
+      email: "a@gmail.com",
+   },
+   {
+      id: 2,
+      email: "b@gmail.com",
+   },
+   {
+      id: 3,
+      email: "c@gmail.com",
+   },
+   {
+      id: 4,
+      email: "d@gmail.com",
+   },
+];
+
+console.log(users[1].email); // b@gmail.com
+console.log(tinderUser); // { id: '123abc', name: 'Mohit', loggedIn: false }
+
+console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'loggedIn' ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'Mohit', false ]
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Mohit' ], [ 'loggedIn', false ] ]
+console.log(tinderUser.hasOwnProperty("loggedIn")); // true
