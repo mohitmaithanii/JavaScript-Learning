3bbebe78a0db6ffcc34717e46098c5730feeb082
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-01-30 19:43:49 +0530

    if, else, else-if

---
+++ 03 Control Flow/01_If_else.js
@@ -0,0 +1,53 @@
+//* if: The `if` statement is used to execute a block of code if a specified condition is true.
+
+const temperature = 41;
+
+if (temperature < 50) {
+   console.log("less than 50 ");
+}
+//* else: The `else` statement is used to execute a block of code if the condition in an `if` statement is false.
+else {
+   console.log("Greater than 50");
+}
+console.log("always execute");
+
+const score = 200;
+
+if (score > 100) {
+   const power = "fly";
+   console.log(`User power: ${power}`);
+}
+
+const balance = 1000;
+
+// instead of using an if statement to check if the balance is greater than 500, simply log "test" to the console
+// (this is just an example of what not to do)
+// if (balance > 500) console.log("test");
+
+//* else-if:  This adds on to the functionality of the `if` statement by allowing for multiple conditions.
+
+if (balance < 500) {
+   console.log("less than 500");
+} else if (balance < 750) {
+   console.log("less than 700");
+} else if (balance < 900) {
+   console.log("less than 900");
+} else {
+   console.log("less than 1000");
+}
+
+const userLoggedIn = true;
+const debitCard = true;
+
+// use an if statement to check if both userLoggedIn and debitCard are true
+if (userLoggedIn && debitCard) {
+   console.log("allowed");
+}
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// use an if statement to check if either loggedInFromGoogle or loggedInFromEmail are true
+if (loggedInFromGoogle || loggedInFromEmail) {
+   console.log("user logged in");
+}
